eb5cadc60395576e7d4ffbb29d73910d4f02bba3
Dave Storey <dave.storey@thetrainline.com>
2022-05-25 10:07:07 +0100

    initial commit

---
+++ NET5/Trainline.TechTest.Api/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Trainline.TechTest.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // do something epic!
+            throw new NotImplementedException();
+        }
+    }
+}
